ce0c44c3bbfcbc29bef26205395b7f9bfed3a06f
Ludovico Massaccesi <62757383+lmassach@users.noreply.github.com>
2021-11-04 12:19:35 +0100

    Working matplotlib utilities and tests. Docs needed.

--- matplotlib/readable_plots.py
+++ matplotlib/readable_plots.py
@@ -1,32 +1,70 @@
+import warnings
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 
-__all__ = ["set_style", "set_symmetric_horizontal_margins"]
+__all__ = ["set_style", "set_symmetric_horizontal_margins", "hide_ticks"]
 
 
-def set_style(font_size=22, tight_layout=False):
-    """Sets matplotlib to have larger text and suitable margins."""
+def _crop(value, max_value, name):
+    if value > max_value:
+        warnings.warn(
+            f"Cropping very large {name} of {value:.2f} to {max_value:.2f}")
+        return max_value
+    return value
+
+
+def set_style(font_size=22, n_rows=1, n_cols=1, auto_layout=False):
+    """
+    Sets matplotlib to have larger text and suitable margins.
+
+    :param font_size: 10 (mpl default) is small, 25 is too large
+    :param n_rows: Number of subplot rows for spacing adjustment.
+        Use 0 for no vertical spacing.
+    :param n_cols: Number of subplot columns for spacing adjustment.
+        Use 0 for no horizontal spacing.
+    :param auto_layout: Turns on automatic margin adjustment. If `True`,
+        other parameters aside from `font_size` will be ignored.
+    """
     # Scales all texts, default size is 10
     mpl.rc('font', size=font_size)
-    if tight_layout:
+    if auto_layout:
         # Uses tight_layout automatically on all p lots
         mpl.rc('figure', autolayout=True)
     else:
         mpl.rc("figure", figsize=(6.4, 4.8), dpi=100, autolayout=False)
-        params = {
-            "left": 0.06 + 0.06 * font_size / 10,
-            "right": 1 - (-0.05 + 0.1 * font_size / 10),
-            "bottom": 0.06 + 0.05 * font_size / 10,
-            "top": 1 - (0.02 + 0.04 * font_size / 10)
-            # TODO Set wspace and hspace
-        }
-        mpl.rc("figure.subplot", **params)
+        # Empirically-tested margins
+        left = 0.06 + 0.06 * font_size / 10
+        left = _crop(left, 0.3, "left margin")
+        right = -0.05 + 0.1 * font_size / 10
+        right = _crop(right, 0.3, "right margin")
+        bottom = 0.06 + 0.05 * font_size / 10
+        bottom = _crop(bottom, 0.3, "bottom margin")
+        top = 0.02 + 0.04 * font_size / 10
+        top = _crop(top, 0.3, "top margin")
+        # Empirically-tested subplot spacings
+        hs = n_rows * (0.15 * font_size / 10)
+        hs = _crop(hs, 1, "vertical spacing")
+        ws = n_cols * (0.15 * font_size / 10)
+        ws = _crop(ws, 1, "horizontal spacing")
+        mpl.rc("figure.subplot", left=left, right=1-right, bottom=bottom,
+               top=1-top, hspace=hs, wspace=ws)
 
 
 def set_symmetric_horizontal_margins():
     """
-    Sets the right margin the same as the left one. Useful when using
-    more than one y-axis scale.
+    Sets the right margin the same as the left one for the current
+    figure. Useful when using more than one y-axis scale.
     """
     plt.subplots_adjust(right=1-mpl.rcParams["figure.subplot.left"])
+
+
+def hide_ticks(ax, which='both'):
+    """
+    Hides the scale ticks (x, y or both) for the given axis. Useful with
+    shared axis in subplots without spacing.
+    """
+    if which in ('x', 'both'):
+        plt.setp(ax.get_xticklabels(), visible=False)
+    if which in ('y', 'both'):
+        plt.setp(ax.get_yticklabels(), visible=False)
--- matplotlib/tests/matplotlib_tests.py
+++ matplotlib/tests/matplotlib_tests.py
@@ -7,6 +7,7 @@ from readable_plots import *
 if __name__ == "__main__":
     with PdfPages('test_results.pdf') as pdf:
         for font_sz in [10, 15, 22]:
+            print(f"Testing font_size={font_sz}")
             # Use the style setup utility
             set_style(font_size=font_sz)
 
@@ -46,5 +47,98 @@ if __name__ == "__main__":
             cb.set_label("Counts / bin")
             pdf.savefig()
 
-            # TODO Subplots
-            # TODO Also test sharex when only one subplot has symmetric margins
+            for spc in [1, 0]:
+                # Subplots (wide)
+                # 2 subplots
+                x = np.linspace(0, 10, 100)
+                y1 = 0.5 + 0.5 * np.sin(x)
+                y2 = y1 + np.random.normal(0, 0.25, x.shape)
+                set_style(font_size=font_sz, n_rows=2*spc)
+                fig = plt.figure()
+                ax1 = plt.subplot(211)
+                ax1.plot(x, y1, '-')
+                ax1.set_title("2 Subplots" if spc else "2 Subplots (no space)")
+                if spc:
+                    ax1.set_xlabel("X Value")
+                else:
+                    hide_ticks(ax1, "x")
+                ax1.set_ylabel("Y Value")
+                plt.grid()
+                ax2 = plt.subplot(212, sharex=ax1)
+                ax2.plot(x, y2, '-')
+                ax2.set_xlabel("X Value")
+                ax2.set_ylabel("Y Value #2")
+                plt.grid()
+                pdf.savefig()
+                # 3 Subplots
+                y3 = y1 + np.random.normal(0, 0.05, x.shape)
+                set_style(font_size=font_sz, n_rows=3*spc)
+                fig = plt.figure()
+                ax1 = plt.subplot(311)
+                ax1.plot(x, y1, '-')
+                ax1.set_title("3 Subplots" if spc else "3 Subplots (no space)")
+                if spc:
+                    ax1.set_xlabel("X Value")
+                else:
+                    hide_ticks(ax1, "x")
+                ax1.set_ylabel("Y Value")
+                plt.grid()
+                ax2 = plt.subplot(312, sharex=ax1)
+                ax2.plot(x, y2, '-')
+                if spc:
+                    ax2.set_xlabel("X Value")
+                else:
+                    hide_ticks(ax2, "x")
+                ax2.set_ylabel("Y Value #2")
+                plt.grid()
+                ax3 = plt.subplot(313, sharex=ax2)
+                ax3.plot(x, y3, '-')
+                ax3.set_xlabel("X Value")
+                ax3.set_ylabel("Y Value #3")
+                plt.grid()
+                pdf.savefig()
+                # Subplots (tall)
+                # 2 subplots
+                set_style(font_size=font_sz, n_cols=2*spc)
+                fig = plt.figure()
+                ax1 = plt.subplot(121)
+                ax1.barh(x, y1, height=0.08)
+                plt.suptitle("2 Subplots" if spc else "2 Subplots (no space)")
+                ax1.set_ylabel("X Value")
+                ax1.set_xlabel("Y Value")
+                plt.grid(axis='x')
+                ax2 = plt.subplot(122, sharey=ax1)
+                ax2.barh(x, y2, height=0.08)
+                if font_sz < 20 and spc:  # Some common sense is still needed
+                    ax2.set_ylabel("X Value")
+                if not spc:
+                    hide_ticks(ax2, 'y')
+                ax2.set_xlabel("Y Value #2")
+                plt.grid(axis='x')
+                pdf.savefig()
+                # 3 Subplots
+                set_style(font_size=font_sz, n_cols=3*spc)
+                fig = plt.figure()
+                ax1 = plt.subplot(131)
+                ax1.barh(x, y1, height=0.08)
+                plt.suptitle("3 Subplots" if spc else "3 Subplots (no space)")
+                ax1.set_ylabel("X Value")
+                ax1.set_xlabel("Y Value")
+                plt.grid(axis='x')
+                ax2 = plt.subplot(132, sharey=ax1)
+                ax2.barh(x, y2, height=0.08)
+                if font_sz < 20 and spc:  # Some common sense is still needed
+                    ax2.set_ylabel("X Value")
+                if not spc:
+                    hide_ticks(ax2, 'y')
+                ax2.set_xlabel("Y Value #2")
+                plt.grid(axis='x')
+                ax3 = plt.subplot(133, sharey=ax2)
+                ax3.barh(x, y3, height=0.08)
+                if font_sz < 20 and spc:  # Some common sense is still needed
+                    ax3.set_ylabel("X Value")
+                if not spc:
+                    hide_ticks(ax3, 'y')
+                ax3.set_xlabel("Y Value #3")
+                plt.grid(axis='x')
+                pdf.savefig()
